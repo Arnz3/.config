d68c9abf2720b3ac6a713dda74b7b48882b3f9c1
Arnz3 <cuyversarno@gmail.com>
2024-12-05 18:30:45 +0100

    add: !! shortcuts form bash

--- fish/config.fish
+++ fish/config.fish
@@ -8,5 +8,32 @@ alias c='clear && cd'
 alias ls='eza --icons'
 alias cls='clear'
 
+# bash !! and !$ keybinds
+
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t -- $history[1]
+            commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -f backward-delete-char history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
+
+
 starship init fish | source
 starship preset bracketed-segments -o ~/.config/starship.toml
